27dbbc10732b329b138e6b55dd5419e123e0f689
Unaisul Hadi <unaisulhadi@gmail.com>
2022-07-19 15:20:51 +0530

    Handling Permissions

---
+++ app/src/main/java/com/hadi/distancetracker/Constants.kt
@@ -0,0 +1,7 @@
+package com.hadi.distancetracker
+
+object Constants {
+
+    const val PERMISSION_LOCATION_REQUEST_CODE = 1
+
+}
---
+++ app/src/main/java/com/hadi/distancetracker/Permissions.kt
@@ -0,0 +1,28 @@
+package com.hadi.distancetracker
+
+import android.Manifest
+import android.content.Context
+import androidx.fragment.app.Fragment
+import com.hadi.distancetracker.Constants.PERMISSION_LOCATION_REQUEST_CODE
+import com.vmadalin.easypermissions.EasyPermissions
+
+object Permissions {
+
+    fun hasLocationPermission(context: Context) : Boolean {
+        return EasyPermissions.hasPermissions(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+
+    fun requestLocationPermission(fragment: Fragment) {
+        EasyPermissions.requestPermissions(
+            fragment,
+            "This app cannot work without Location Permission",
+            PERMISSION_LOCATION_REQUEST_CODE,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+}
