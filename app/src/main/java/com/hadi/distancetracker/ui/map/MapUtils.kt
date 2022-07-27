84e7700d5fed57f00aa0c408281d23cb301779bc
Unaisul Hadi <unaisulhadi@gmail.com>
2022-07-27 11:27:13 +0530

    Calculate DIstance

--- app/src/main/java/com/hadi/distancetracker/ui/map/MapUtils.kt
+++ app/src/main/java/com/hadi/distancetracker/ui/map/MapUtils.kt
@@ -2,24 +2,36 @@ package com.hadi.distancetracker.ui.map
 
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+import java.text.DecimalFormat
 
 object MapUtils {
 
-    fun setCameraPosition(location :LatLng) : CameraPosition {
+    fun setCameraPosition(location: LatLng): CameraPosition {
         return CameraPosition.Builder()
             .target(location)
             .zoom(18f)
             .build()
     }
 
-    fun calculateElapsedTime(startTime: Long, stopTime : Long) : String{
+    fun calculateElapsedTime(startTime: Long, stopTime: Long): String {
         val elapsedTime = stopTime - startTime
         val seconds = (elapsedTime / 1000).toInt() % 60
-        val minutes = (elapsedTime / (1000 * 60) % 60 )
+        val minutes = (elapsedTime / (1000 * 60) % 60)
         val hours = (elapsedTime / (1000 * 60 * 60)) % 24
 
 
         return "$hours:$minutes:$seconds"
+    }
+
+    fun calculateDistance(locationList: MutableList<LatLng>): String {
+        if (locationList.isNotEmpty()) {
+            val meters =
+                SphericalUtil.computeDistanceBetween(locationList.first(), locationList.last())
+            val kiloMeters = meters/1000;
+            return DecimalFormat("#.##").format(kiloMeters)
+        }
+        return "0.00"
     }
 
 }
